ee089b63f93c0775155e7074dfad3a08cbd5b9a1
Zhafran Pratama <muhamzhafranpratama03@gmail.com>
2025-12-04 15:27:00 +0700

    adding primitive for  mll 1-n function

--- melodic.cpp
+++ melodic.cpp
@@ -7,3 +7,63 @@ void testHeader(){
 void testBranchGit(){
     cout << "ini buat test doang";
 }
+
+void createListUser(userList &L){
+    L.first = nullptr;
+    L.last = nullptr;
+}
+
+bool isEmptyUser(userList L){
+    return L.first == nullptr && L.last == nullptr;
+}
+
+bool isEmptyPlaylist(adrUser p){
+    return p->firstPlaylist == nullptr;
+}
+
+
+adrUser createElemenUser(string username, string password, adrPlaylist p){
+    adrUser u;
+    u = new elmUser;
+
+    u->username = username;
+    u->password = password;
+    u->next = nullptr;
+    u->prev = nullptr;
+    u->firstPlaylist = p;
+
+    return u;
+}
+
+adrPlaylist createElemenPlaylist(string namaPlaylist){
+    adrPlaylist q;
+    q = new elmPlaylist;
+
+    q->namaPlaylist= namaPlaylist;
+    q->firstSong = nullptr;
+    q->next = nullptr;
+    q->prev = nullptr;
+
+    return q;
+}
+
+void addUser(userList &L, adrUser p){
+    if (isEmptyUser(L)) {
+        L.first = p;
+        L.last = p;
+    }else {
+        L.last->next = p;
+        p->prev = L.last;
+        L.last = p;
+    }
+}
+
+void addPlaylist(adrUser &p, adrPlaylist q){
+    adrPlaylist r;
+    r = p->firstPlaylist;
+    while (r->next != nullptr) {
+        r = r->next;
+    }
+    r->next = q;
+    q->prev = r;
+}
